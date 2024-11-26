7f3371cc09359447bfc054c0aac5a50586a1f218
Iskatiboi <scottfranklinmaher@gmail.com>
2024-11-26 23:31:28 +0800

    Refactored Main method

---
+++ PAIR ACTIVITY OOP/Pair_Activity/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pair_Activity
+{
+
+    static void Main(string[] args)
+    {
+        Calculate[] shapes = new Calculate[]
+        {
+                new Rectangle { Length = 10, Width = 5 },
+                new Circle { Radius = 7 },
+                new Triangle { Base = 24, Height = 5 }
+        };
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine("Area: " + shape.CalculateArea());
+        }
+
+        Console.ReadLine();
+    }
+}
+
+
